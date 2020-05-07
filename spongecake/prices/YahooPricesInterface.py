2252de7467ca1dfa3c4db624d36e90fedc1912e2
cakers <chris.j.akers@gmail.com>
2020-05-07 21:35:11 +0100

    Added market parameter to download yahoo prices

--- spongecake/prices/YahooPricesInterface.py
+++ spongecake/prices/YahooPricesInterface.py
@@ -9,7 +9,7 @@ class YahooPricesInterface(PricesInterface):
 
     prices_cache = {}
 
-    def get_yahoo_prices(self, tidm, from_date=(date.today() - timedelta(days=365)), to_date=date.today(), force_cache_refresh=False):
+    def get_yahoo_prices(self, tidm, market='L', from_date=(date.today() - timedelta(days=365)), to_date=date.today(), force_cache_refresh=False):
         if tidm in self.prices_cache.keys() and force_cache_refresh is False:
             logging.info('Found {0} in Yahoo Historic Prices cache returning that instead'.format(tidm))
             return self.prices_cache[tidm]
@@ -18,10 +18,10 @@ class YahooPricesInterface(PricesInterface):
         to_date_str = to_date.strftime('%Y-%m-%d')
         logging.info('Attempting to download historic prices for [{0}] between dates [{1}] and [{2}] from YAHOO'.format(tidm, from_date_str, to_date_str))
         try:
-            df_prices = pdr.DataReader(tidm, 'yahoo', from_date_str, to_date_str)
-            logging.info('Downloaded [{0}] prices for [{1}]'.format(len(df_prices), tidm))
+            df_prices = pdr.DataReader('{0}.{1}'.format(tidm,market), 'yahoo', from_date_str, to_date_str)
+            logging.info('Downloaded [{0}] prices for [{1}.{2}]'.format(len(df_prices), tidm, market))
         except:
-            logging.error('Unable to download prices for [{0}], you\'ll have to do this manually'.format(tidm))
+            logging.error('Unable to download prices for [{0}.{1}], you\'ll have to do this manually'.format(tidm, market))
             return pd.DataFrame()
         df_prices = self._remove_duplicate_rows_from_dataframe(df_prices)
 
